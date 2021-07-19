af2873c170c18c25a9e3e9b8ac9a78517cd4c188
Astyle-396 <87637704+Astyle-396@users.noreply.github.com>
2021-07-19 13:20:49 +0530

    Add files via upload
    add

---
+++ list.py
@@ -0,0 +1,15 @@
+a = [ 1,2,3]
+
+print(f"Simple List {a}")
+
+a.append(4)
+
+print(f"added 4  {a}")
+
+a.sort()
+
+print(f"sorted {a}")
+
+# print(f"sorted {count_}")
+
+print(f"sorted {a.index(3)}")
